c7d7d486fc65fe252c18c0f1b1b42bb4c90199c8
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-06-29 19:24:29 +0000

    lab28

--- snacks/views.py
+++ snacks/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, TemplateView,CreateView, UpdateView, DeleteView
 from .models import Snack
+from django.urls import reverse_lazy
 
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = "home.html"
+
 class SnackListView(ListView):
-    template_name = 'home.html'
+    template_name = "snack_list.html"
     model = Snack
     context_object_name = "snacks"
 
 
 class SnackDetailView(DetailView):
-    template_name = 'detail.html'
+    template_name = "snack_detail.html"
     model = Snack
+
+class SnackCreateView (CreateView):
+    template_name='snack_create.html'
+    model=Snack
+    fields= ['name', 'purchaser', 'desc']
+
+class SnackUpdateView (UpdateView):
+    template_name='snack_update.html'
+    model=Snack
+    fields="__all__"
+    success_url=reverse_lazy('snack_list')
+
+class SnackDeleteView(DeleteView):
+    template_name = 'snack_delete.html'
+    model = Snack
+    success_url = reverse_lazy('snack_list')
